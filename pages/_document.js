5a123311c8c228d0e90caeeee2eaa5d9680f5e2c
Juliana Costa <107275594+blendibox@users.noreply.github.com>
2023-04-07 08:15:25 -0300

    clarity

--- pages/_document.js
+++ pages/_document.js
@@ -55,7 +55,7 @@ class MyDocument extends Document {
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "d6c6uyqopa");
+            })(window, document, "clarity", "script", "gkseha70hf");
          ` }}
           />
             
