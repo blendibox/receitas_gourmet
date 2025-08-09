484d287d0e486b2c6ad519c8016b35c170b55bbe
blendibox <loja@blendibox.com.br>
2025-08-09 15:20:40 -0300

    rodape

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from '../components/footer.js'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+		<Footer></Footer>
       </body>
     </html>
   );
---
+++ components/footer.js
@@ -0,0 +1,41 @@
+'use client';
+
+import Link from 'next/link';
+import Image from  'next/image';
+import { useRouter } from 'next/navigation';
+
+export default function Footer() {
+  const router = useRouter();
+  const ano = new Date().getFullYear();
+
+  const navegarComScroll = (href) => {
+    router.push(href);
+    setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }, 100); // pequena espera para a navegação acontecer
+  };
+
+  return (
+    <footer className=" min-h-[120px] bg-black text-white ">
+      <div className="max-w-7xl mx-auto px-4 py-10 grid grid-cols-1 md:grid-cols-4 gap-8">
+
+		<div>
+          <h3 className="text-sm font-semibold mb-2 uppercase text-gray-100">Cursos</h3>
+          <ul className="space-y-1 text-sm">
+           <li><Link name='Todo os Cursos' href="https://cursoconfeitaria.blendibox.com.br/" className="hover:underline text-gray-100">Todo os Cursos</Link></li>
+		  <li><Link name='Cursos da Chef Isis Alvarez' href="https://cursoconfeitaria.blendibox.com.br/chefisisalvarez" className="hover:underline text-gray-100">Cursos da Chef Isis Alvarez</Link></li>
+		  <li><Link name='Cursos da Chef Marrara Bortoloti' href="https://cursoconfeitaria.blendibox.com.br/chefmarrarabortoloti" className="hover:underline text-gray-100">Cursos da Chef Marrara Bortoloti</Link></li>
+		
+		  </ul>
+        </div>
+
+       
+
+      </div>
+
+      <div className="border-t border-gray-700 py-4 text-center text-sm text-gray-100">
+        © {ano} <Link href="https://www.blendibox.com.br">Blendibox. Todos os direitos reservados.</Link>
+      </div>
+    </footer>
+  );
+}
